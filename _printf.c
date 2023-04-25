b39863e6560a377fff89c210dd70d143686933ba
Oum01 <slimani-oumaima@outlook.com>
2023-04-25 12:30:34 -0700

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -4,52 +4,40 @@
  * @format: format string containing the characters and the specifiers
  * Return: length of the formatted output string
  */
+
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-		while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-		format++;
-		if (*format == 'c')
-		{
-			char c = va_arg(args, int);
+    while (*format != '\0') {
+        if (*format == '%') {
+            format++;
+            if (*format == 'c') {
+                char c = va_arg(args, int);
+                putchar(c);
+                count++;
+            } else if (*format == 's') {
+                char *s = va_arg(args, char *);
+                while (*s != '\0') {
+                    putchar(*s);
+                    s++;
+                    count++;
+                }
+            } else if (*format == '%') {
+                putchar('%');
+                count++;
+            }
+        } else {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
 
-			_putchar(c);
-			count++;
-		}
-		else if (*format == 's')
-		{
-			char *s = va_arg(args, char *);
+    va_end(args);
 
-			while (*s != '\0')
-			{
-				_putchar(*s);
-				s++;
-				count++;
-			}
-		}
-		else if (*format == '%')
-		{
-			_putchar('%');
-			count++;
-		}
-		}
-		else if (*format == ' ')
-		{
-		}
-		else
-		{
-			_putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+    return count;
 }
