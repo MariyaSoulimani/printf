7fdfce8ce1e665ea56a17feba142d1868bd5e851
Oum01 <slimani-oumaima@outlook.com>
2023-04-25 11:54:31 -0700

    function that produces output according to a format.

--- _printf.c
+++ _printf.c
@@ -6,37 +6,37 @@
  */
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
+	va_list args;
+	int count = 0;
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (*format != '\0')
-        {
-                if (*format == '%')
-                {
-                        format++;
-                if (*format == 'c')
-                {
-                char c = va_arg(args, int);
-
-                putchar(c);
-                count++;
-                }
-                else if (*format == 's')
-                {
-                        char *s = va_arg(args, char *);
-
-                        while (*s != '\0')
-                        {
-                                putchar(*s);
-                                s++;
-                                count++;
-                        }
-                }
-                else if (*format == '%')
+		while (*format != '\0')
+	{
+		if (*format == '%')
 		{
-			putchar('%');
+		format++;
+		if (*format == 'c')
+		{
+			char c = va_arg(args, int);
+
+			_putchar(c);
+			count++;
+		}
+		else if (*format == 's')
+		{
+			char *s = va_arg(args, char *);
+
+			while (*s != '\0')
+			{
+				_putchar(*s);
+				s++;
+				count++;
+			}
+		}
+		else if (*format == '%')
+		{
+			_putchar('%');
 			count++;
 		}
 		}
@@ -45,7 +45,7 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			putchar(*format);
+			_putchar(*format);
 			count++;
 		}
 		format++;
